05cd1f15b78b2fe1da41234bd3c4b2f53a1d0ecb
admytruk79-netizen <admytruk79@gmail.com>
2026-08-24 12:55:05 -0700

    Expose single Local map instance to location state

--- public/js/neutral-map-bootstrap.js
+++ public/js/neutral-map-bootstrap.js
@@ -8,6 +8,7 @@ class RoviqNeutralMap extends BaseMap{
     const isLegacyPortland=center&&Math.abs(Number(center[0])+122.6765)<0.01&&Math.abs(Number(center[1])-45.5231)<0.01;
     const safe=isLegacyPortland?{...options,center:[0,20],zoom:2.4,pitch:0,bearing:0}:options;
     super(safe);
+    window.__ROVIQ_MAP=this;
   }
 }
 RoviqNeutralMap.__roviqNeutralBootstrap=true;
